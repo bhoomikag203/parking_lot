4f6394cd3ff62b162a5a2b701fb270a0befdeb7d
bhoomikag203 <bhoomikag203@gmail.com>
2019-07-26 18:04:12 +0530

    Refactor. should not park a vehicle when parking lot is full

--- parkinglot.go
+++ parkinglot.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"errors"
+)
+
+var ErrParkinglotFull = errors.New("Cannot park a vehicle as parking lot is full")
+
 type parkingLot struct {
 	capacity int
 	occupied int
@@ -9,8 +15,12 @@ func (p parkingLot) NewParkingLot(capacity int) parkingLot {
 	return p
 }
 
-func (p *parkingLot) Park(vehicle string) {
+func (p *parkingLot) Park(vehicle string) error {
+	if p.IsFull() {
+		return ErrParkinglotFull
+	}
 	p.occupied += 1
+	return nil
 }
 
 func (p *parkingLot) Unpark(vehicle string) {
--- parkinglot_test.go
+++ parkinglot_test.go
@@ -38,15 +38,20 @@ func TestPark(t *testing.T) {
 		}
 	})
 
-	/*t.Run("should not park a vehicle w", func(t *testing.T) {
-		pl := parkingLot{capacity: 10, occupied: 0}
+	t.Run("should not park a vehicle when parking lot is full", func(t *testing.T) {
+		pl := parkingLot{capacity: 1, occupied: 1}
+		want := "Cannot park a vehicle as parking lot is full"
 
-		pl.Park("vehicle1")
+		got := pl.Park("vehicle1")
 
-		if pl.occupied != 1 {
-			t.Errorf("Couldn't park a vehicle. got %v want %v", pl.occupied, 1)
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
 		}
-	})*/
+
+		if got.Error() != want {
+			t.Errorf(" Got %v want %v", got, want)
+		}
+	})
 }
 
 func TestUnpark(t *testing.T) {
